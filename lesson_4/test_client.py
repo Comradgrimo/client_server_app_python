2ca55930c7ba140f88ec6a20174c2debb2139248
Dmitriy <comradgrimo@gmail.com>
2021-05-18 17:38:32 +0300

    add task_4

---
+++ lesson_4/test_client.py
@@ -0,0 +1,17 @@
+import unittest
+# from lesson_3.response import ServerResponse
+# from lesson_3.client import send_json
+import json
+
+
+def send_json(arg: dict) -> bytes:
+    return json.dumps(arg).encode('utf-8')
+
+class TestClient(unittest.TestCase):
+
+    def test_send_json(self):
+        foo = {'grimo':1}
+        self.assertEqual(send_json(foo), b'{"grimo": 1}')
+
+if __name__ == "__main__":
+    unittest.main()
